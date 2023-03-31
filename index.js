47598495dcb1323e3476d93fed68a45ba821a30b
+ <+>
2023-03-31 08:08:22 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function moveDodgerLeft() {
+    var left = parseInt(dodger.style.left)
+    if (left > 0) {
+      dodger.style.left = (left - 1) + 'px'
+    }
+  }
+  
+  function moveDodgerRight() {
+    var left = parseInt(dodger.style.left)
+    if (left < (400 - 40)) {
+      dodger.style.left = (left + 1) + 'px'
+    }
+  }
+  
